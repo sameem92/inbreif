7d02157ecf7fd2666aecdc9e1687b8ac3c72ab6c
sameem92 <sameem.alturk2@gmail.com>
2025-01-01 08:39:51 +0400

    comments

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,6 @@
 export const dynamicBlurDataUrl = `data:image/svg+xml;base64,${Buffer.from(
   `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="10" viewBox="0 0 100 50" preserveAspectRatio="none">
   
- 
   
 
   
