ce6de07ec765dbe6370b5404571666b7c3f164d7
Abdelrahman <abdoezzat098@gmail.com>
2025-02-18 16:54:39 +0200

    fixing currency alignment in en and de

--- src/app/about-us/page.tsx
+++
@@ -1,9 +0,0 @@
-import Home from "@/components/about/Home"
-
-export const metadata = {
-  title: "من نحن | شركة إن بريف للبرمجة والتسويق",
-}
-
-export default function Main() {
-  return <Home />
-}
--- src/app/contact-us/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContactUsComponent from "@/components/contactUs/contactUs"
-
-export const metadata = {
-  title: "تواصل معنا | شركة إن بريف للبرمجة والتسويق",
-}
-
-export default function ContactUsPage() {
-  return <ContactUsComponent />
-}
