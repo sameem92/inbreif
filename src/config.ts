a39b43d427578d3f96ec2f2e0559f88cdbad0bd3
Abdelrahman <abdoezzat098@gmail.com>
2025-03-15 22:44:03 +0200

    changing marketing image

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-export const locales = ["en", "ar", "de"] as const;
 export const port = process.env.PORT || 3000;
 export const host = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
